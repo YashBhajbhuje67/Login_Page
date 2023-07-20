d23a9a4bd83fba5f9074ed5a91f2deff348eadcc
YashBhajbhuje67 <yashbhajbhuje67@gmail.com>
2023-07-20 16:28:02 +0530

    Modified Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Display.css';
 
 const Display = ({userName}) => {
   return (
-    <div>Welcome {userName} !</div>
+    <div className='display'>Welcome {userName} !</div>
   )
 }
 
